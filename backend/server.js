692d6305fd99c201e2a4b7dde91f049dfdaf44bb
Mahtab <mahtabali2k20@gmail.com>
2025-08-25 22:47:37 -0700

    connect mongoDB to backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats }  = require("./data/data");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db.js");
+const color = require("colors");
+
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-dotenv.config();
+
+
+
 
 // let port = 5000;
 
@@ -27,5 +36,5 @@ app.get("/api/chats/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000,console.log(`Server start on port ${PORT}`))
+app.listen(5000,console.log(`Server start on port ${PORT}`.yellow.bold))
 
